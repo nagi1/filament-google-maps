2c62563e34427d92309a38c704024bbfa02e0112
Marc-Antoine Favreau <8480549+Heyian@users.noreply.github.com>
2024-08-28 14:31:10 -0400

    feat: Update maps column view to allow the use of placeholders
    See Pull Request #119 on original repo from ryanmortier

--- resources/views/columns/filament-google-maps-column.blade.php
+++ resources/views/columns/filament-google-maps-column.blade.php
@@ -3,25 +3,46 @@
     'px-4 py-3' => ! $isInline(),
 ]) }}>
     @php
-        $height = $getHeight();
-        $width = $getWidth();
+        use Filament\Support\Enums\Alignment;
+
+            $height = $getHeight();
+            $width = $getWidth();
+            $alignment = $getAlignment();
+
+        if (! $alignment instanceof Alignment) {
+            $alignment = filled($alignment) ? (Alignment::tryFrom($alignment) ?? $alignment) : null;
+        }
     @endphp
 
     <div
-        style="
+            @class([
+              'flex items-center',
+              match ($alignment) {
+                Alignment::Start, Alignment::Left => 'text-start justify-start',
+                Alignment::Center => 'text-center justify-center',
+                Alignment::End, Alignment::Right => 'text-end justify-end',
+                Alignment::Between, Alignment::Justify => 'text-justify justify-between',
+                default => $alignment,
+              },
+        ])
+            style="
             {!! $height !== null ? "height: {$height}px;" : null !!}
             {!! $width !== null ? "width: {$width}px;" : null !!}
         "
     >
         @if ($path = $getImagePath())
             <img
-                src="{{ $path }}"
-                style="
+                    src="{{ $path }}"
+                    style="
                     {!! $height !== null ? "height: {$height}px;" : null !!}
                     {!! $width !== null ? "width: {$width}px;" : null !!}
                 "
-                {{ $getExtraImgAttributeBag() }}
+                    {{ $getExtraImgAttributeBag() }}
             >
-       @endif
+        @elseif (($placeholder = $getPlaceholder()) !== null)
+            <x-filament-tables::columns.placeholder>
+                {{ $placeholder }}
+            </x-filament-tables::columns.placeholder>
+        @endif
     </div>
 </div>
